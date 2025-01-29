1de5e6b0cea6a8bc5904de2665287048b1bb8ae1
filipe <felepesilvagalvao@gmail.com>
2025-01-29 13:19:15 -0300

    init commit

---
+++ src/javascript/menu-script.js
@@ -0,0 +1,9 @@
+const buttonBars = document.querySelector('#menu-mobile > button') 
+
+buttonBars.addEventListener('click', (e)=>{
+    const navMobile = e.target.parentNode.parentNode.children[1].firstElementChild
+    navMobile.classList.toggle('hide')
+    navMobile.classList.toggle('show')
+    buttonBars.querySelector('i').classList.toggle('fa-bars')
+    buttonBars.querySelector('i').classList.toggle('fa-x')
+})

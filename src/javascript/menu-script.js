247102dd8cdf23b31147aa99c1715f3ebd8501ea
filipe <felepesilvagalvao@gmail.com>
2025-01-29 18:39:14 -0300

    tranform in module and export code

--- src/javascript/menu-script.js
+++ src/javascript/menu-script.js
@@ -1,9 +1,11 @@
-const buttonBars = document.querySelector('#menu-mobile > button') 
+export function menu() {
+    const buttonBars = document.querySelector('#menu-mobile > button')
 
-buttonBars.addEventListener('click', (e)=>{
-    const navMobile = e.target.parentNode.parentNode.children[1].firstElementChild
-    navMobile.classList.toggle('hide')
-    navMobile.classList.toggle('show')
-    buttonBars.querySelector('i').classList.toggle('fa-bars')
-    buttonBars.querySelector('i').classList.toggle('fa-x')
-})
+    buttonBars.addEventListener('click', (e) => {
+        const navMobile = e.target.parentNode.parentNode.children[1].firstElementChild
+        navMobile.classList.toggle('hide')
+        navMobile.classList.toggle('show')
+        buttonBars.querySelector('i').classList.toggle('fa-bars')
+        buttonBars.querySelector('i').classList.toggle('fa-x')
+    })
+}

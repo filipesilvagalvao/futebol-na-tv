a930ce943435aca1b00057c6d510bccf240e64b7
filipe <felepesilvagalvao@gmail.com>
2025-02-24 19:33:06 -0300

    import de features

--- src/javascript/codes.js
+++ src/javascript/codes.js
@@ -1,10 +1,17 @@
 import { menu } from "/src/javascript/menu-script.js";
 import { madeGames } from "/src/javascript/callgames.js";
 import { inlive } from "/src/javascript/inlive.js"
+import { searchChannel } from "/src/javascript/search.js"
+import { closeModal } from "/src/javascript/modalResults.js"
 
 menu()
 madeGames()
 inlive()
+searchChannel()
+closeModal()
+
+
+
 
 
 
